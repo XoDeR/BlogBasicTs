83bed0b0c0f722551ca8fc4e2b78bad34ea55dd4
Volodymyr <XoDeR@users.noreply.github.com>
2023-10-18 12:17:04 +0200

    Basic Express with TypeScript setup

---
+++ server/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+const app: express.Application = express();
+const port: number = 7788;
+ 
+app.get('/', (_req, _res) => {
+    _res.send("TypeScript With Express");
+});
+ 
+app.listen(port, () => {
+    console.log(`TypeScript with Express 
+         http://localhost:${port}/`);
+});
